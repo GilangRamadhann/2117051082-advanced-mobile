cbce05f99d036ce09ee0a10408f4df082b96647c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-10-15 09:16:07 +0700

    belajar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:mola/screens/home_screen.dart';
 import 'package:mola/screens/onboarding_screen.dart';
 import 'package:mola/screens/form_screen.dart';
+import 'package:mola/screens/root_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const RootScreen(),
       //   home: Scaffold(a
       //     body: Center(
       //       child: Text("Test"),
